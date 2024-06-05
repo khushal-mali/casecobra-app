3e76130a2e8a778bdd796a72a0252bdcf88d8e42
Khushal Mali <khushalmali8448@gmail.com>
2024-06-05 23:23:24 +0530

    reviews on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import { Icons } from "@/components/icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
 import { Reviews } from "@/components/Reviews";
-import { Check, Star } from "lucide-react";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowRight, Check, Star } from "lucide-react";
 
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -15,6 +17,7 @@ export default function Home() {
             <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
               <div className="absolute w-28 left-0 -top-20 hidden lg:block">
                 <img src="/snake-1.png" alt="Snake image" className="w-full" />
+                {/* <div className="pointer-events-none absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-slate-50/90" /> */}
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
                 Your Image on{" "}
@@ -208,6 +211,73 @@ export default function Home() {
         <div className="pt-16">
           <Reviews />
         </div>
+
+        <section>
+          <MaxWidthWrapper className="py-24">
+            <div className="mb-12 px-6 lg:px-8">
+              <div className="mx-auto max-w-2xl sm:text-center">
+                <h2 className="order-1 mt-2 tracking-tight text-center text-balance !leading-tight font-bold text-5xl md:text-6xl text-gray-900">
+                  Upload your photo and get{" "}
+                  <span className="relative px-2 bg-green-600 text-white">
+                    your own case
+                  </span>{" "}
+                  now
+                </h2>
+              </div>
+            </div>
+
+            <div className="mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="relative flex flex-col items-center md:grid grid-cols-2 gap-40">
+                <img
+                  src="/arrow.png"
+                  alt="arrow image"
+                  className="absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0"
+                />
+
+                <div className="relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl">
+                  <img
+                    src="/horse.jpg"
+                    className="rounded-md object-cover bg-white shadow-2xl ring-1 ring-gray-900/10 h-full w-full"
+                    alt="horse image"
+                  />
+                </div>
+
+                <Phone className="w-60" imgSrc="/horse_phone.jpg" />
+              </div>
+            </div>
+
+            <ul className="mx-auto mt-12 max-w-prose sm:text-lg space-y-2 w-fit">
+              <li className="w-fit">
+                <Check className="h-5 w-5 text-green-600 inline mr-1.5" />{" "}
+                High-quality silicon material
+              </li>
+              <li className="w-fit">
+                <Check className="h-5 w-5 text-green-600 inline mr-1.5" />{" "}
+                Scratch and fingerprint resistant coating
+              </li>
+              <li className="w-fit">
+                <Check className="h-5 w-5 text-green-600 inline mr-1.5" />{" "}
+                Wireless charging compatible
+              </li>
+              <li className="w-fit">
+                <Check className="h-5 w-5 text-green-600 inline mr-1.5" /> 5
+                year print warranty
+              </li>
+
+              <div className="flex justify-center">
+                <Link
+                  href="/configure/upload"
+                  className={buttonVariants({
+                    size: "lg",
+                    className: "mx-auto mt-8",
+                  })}
+                >
+                  Create your case now <ArrowRight className="h-4 w-4 ml-1.5" />{" "}
+                </Link>
+              </div>
+            </ul>
+          </MaxWidthWrapper>
+        </section>
       </section>
     </div>
   );
