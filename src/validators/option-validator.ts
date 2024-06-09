8aba701f883f45a8b61e12f2242b06dd51d53c3c
Khushal Mali <khushalmali8448@gmail.com>
2024-06-09 11:49:43 +0530

    colors

---
+++ src/validators/option-validator.ts
@@ -0,0 +1,5 @@
+export const COLORS = [
+  { label: "Black", value: "black", tw: "zinc-900" },
+  { label: "Blue", value: "blue", tw: "blue-950" },
+  { label: "Rose", value: "rose", tw: "rose-950" },
+] as const;
