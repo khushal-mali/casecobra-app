54db3cbfe2467509690381d50995cdc1375f44f7
Khushal Mali <khushalmali8448@gmail.com>
2024-06-09 10:47:31 +0530

    next image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["utfs.io"],
+    remotePatterns: [
+      {
+        hostname: "utfs.io",
+      },
+    ],
   },
 };
 
