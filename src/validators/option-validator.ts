489619fbab3b185b3ccf166f27033e50964ea7d5
Khushal Mali <khushalmali8448@gmail.com>
2024-06-09 12:37:20 +0530

    colors

--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -1,3 +1,7 @@
+// bg-blue-950 border-blue-950
+// bg-zinc-900 border-zinc-900
+// bg-rose-950 border-rose-950
+
 export const COLORS = [
   { label: "Black", value: "black", tw: "zinc-900" },
   { label: "Blue", value: "blue", tw: "blue-950" },
