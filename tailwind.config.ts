97c523d58a9c16b5c4e97d8753fc35f6ae9d7f59
Khushal Mali <khushalmali8448@gmail.com>
2024-06-11 18:09:09 +0530

    flashing animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -72,10 +72,15 @@ const config = {
             transform: "translateY(-50%)",
           },
         },
+        flashing: {
+          "0%, 100%": { opacity: "0.2" },
+          "20%": { opacity: "1" },
+        },
       },
       animation: {
         marquee: "marquee var(--marquee-duration) linear infinite",
         "fade-in": "fade-in 0.5s linear forwards",
+        flashing: "flashing 1.4s infinite linear",
       },
     },
   },
