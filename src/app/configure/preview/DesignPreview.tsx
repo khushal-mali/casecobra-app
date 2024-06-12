15c89ef477a4d3c6f464b093a0a9553dfc5c2342
Khushal Mali <khushalmali8448@gmail.com>
2024-06-12 14:36:07 +0530

    design preview component

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -4,13 +4,25 @@ import Phone from "@/components/Phone";
 import { Button } from "@/components/ui/button";
 import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
 import { cn, formatPrice } from "@/lib/utils";
-import { COLORS, MODELS } from "@/validators/option-validator";
-import type { Configuration } from "@prisma/client";
+import { COLORS, FINISHES, MODELS } from "@/validators/option-validator";
+import { Configuration } from "@prisma/client";
+import { useMutation } from "@tanstack/react-query";
 import { ArrowRight, Check } from "lucide-react";
 import { useEffect, useState } from "react";
 import Confetti from "react-dom-confetti";
+import { createCheckoutSession } from "./actions";
+import { useRouter } from "next/navigation";
+import { useToast } from "@/components/ui/use-toast";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+// import LoginModal from "@/components/LoginModal";
 
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
+  const router = useRouter();
+  const { toast } = useToast();
+  const { id } = configuration;
+  const { user } = useKindeBrowserClient();
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState<boolean>(false);
+
   const [showConfetti, setShowConfetti] = useState<boolean>(false);
   useEffect(() => setShowConfetti(true), []);
 
@@ -25,62 +37,85 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   )!;
 
   let totalPrice = BASE_PRICE;
-
   if (material === "polycarbonate")
     totalPrice += PRODUCT_PRICES.material.polycarbonate;
-
   if (finish === "textured") totalPrice += PRODUCT_PRICES.finish.textured;
+
+  const { mutate: createPaymentSession } = useMutation({
+    mutationKey: ["get-checkout-session"],
+    mutationFn: createCheckoutSession,
+    onSuccess: ({ url }) => {
+      if (url) router.push(url);
+      else throw new Error("Unable to retrieve payment URL.");
+    },
+    onError: () => {
+      toast({
+        title: "Something went wrong",
+        description: "There was an error on our end. Please try again.",
+        variant: "destructive",
+      });
+    },
+  });
+
+  const handleCheckout = () => {
+    if (user) {
+      // create payment session
+      createPaymentSession({ configId: id });
+    } else {
+      // need to log in
+      localStorage.setItem("configurationId", id);
+      setIsLoginModalOpen(true);
+    }
+  };
 
   return (
     <>
       <div
-        aria-hidden={true}
+        aria-hidden="true"
         className="pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center"
       >
         <Confetti
           active={showConfetti}
-          config={{
-            elementCount: 200,
-            spread: 90,
-          }}
+          config={{ elementCount: 200, spread: 90 }}
         />
       </div>
 
-      <div className="mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
-        <div className="sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2">
+      {/* <LoginModal isOpen={isLoginModalOpen} setIsOpen={setIsLoginModalOpen} /> */}
+
+      <div className="mt-20 flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12">
+        <div className="md:col-span-4 lg:col-span-3 md:row-span-2 md:row-end-2">
           <Phone
-            className={cn(`bg-${tw}`)}
+            className={cn(`bg-${tw}`, "max-w-[150px] md:max-w-full")}
             imgSrc={configuration.croppedImageUrl!}
           />
         </div>
 
-        <div className="mt-6 sm:col-span-9 sm:mt-0 md:row-end-1">
+        <div className="mt-6 sm:col-span-9 md:row-end-1">
           <h3 className="text-3xl font-bold tracking-tight text-gray-900">
             Your {modelLabel} Case
           </h3>
-
           <div className="mt-3 flex items-center gap-1.5 text-base">
-            <Check className="h-4 w-4 text-green-400" />
+            <Check className="h-4 w-4 text-green-500" />
             In stock and ready to ship
           </div>
         </div>
 
         <div className="sm:col-span-12 md:col-span-9 text-base">
           <div className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10">
-            <div className="">
-              <p className="font-medium text-zinc-900">Highlight</p>
+            <div>
+              <p className="font-medium text-zinc-950">Highlights</p>
               <ol className="mt-3 text-zinc-700 list-disc list-inside">
                 <li>Wireless charging compatible</li>
-                <li>TPU Shock absorption</li>
+                <li>TPU shock absorption</li>
                 <li>Packaging made from recycled materials</li>
                 <li>5 year print warranty</li>
               </ol>
             </div>
             <div>
-              <p className="font-medium text-zinc-900">Materials</p>
+              <p className="font-medium text-zinc-950">Materials</p>
               <ol className="mt-3 text-zinc-700 list-disc list-inside">
-                <li>High quality, duration material</li>
-                <li>Scratch and fingerprint resistant coating</li>
+                <li>High-quality, durable material</li>
+                <li>Scratch- and fingerprint resistant coating</li>
               </ol>
             </div>
           </div>
@@ -106,7 +141,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
 
                 {material === "polycarbonate" ? (
                   <div className="flex items-center justify-between py-1 mt-2">
-                    <p className="text-gray-600">Soft Polycarbonate material</p>
+                    <p className="text-gray-600">Soft polycarbonate material</p>
                     <p className="font-medium text-gray-900">
                       {formatPrice(PRODUCT_PRICES.material.polycarbonate / 100)}
                     </p>
@@ -125,7 +160,10 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
             </div>
 
             <div className="mt-8 flex justify-end pb-12">
-              <Button className="px-4 sm:px-6 lg:px-8">
+              <Button
+                onClick={() => handleCheckout()}
+                className="px-4 sm:px-6 lg:px-8"
+              >
                 Check out <ArrowRight className="h-4 w-4 ml-1.5 inline" />
               </Button>
             </div>
