9d249910e2ba4cfc420aac3c4bab60a4a0ff778e
Khushal Mali <khushalmali8448@gmail.com>
2024-06-12 22:30:07 +0530

    payment method edit

--- src/app/configure/preview/actions.ts
+++ src/app/configure/preview/actions.ts
@@ -68,7 +68,7 @@ export const createCheckoutSession = async ({
   const stripeSession = await stripe.checkout.sessions.create({
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
-    payment_method_types: ["card", "paypal", "amazon_pay"],
+    payment_method_types: ["card"],
     mode: "payment",
     shipping_address_collection: { allowed_countries: ["IN", "US"] },
     metadata: {
