784a0d7ab01028d7cd7f95ca7ae72fa8d46a8d2c
Khushal Mali <khushalmali8448@gmail.com>
2024-06-13 18:37:24 +0530

    q mark removed

--- src/app/auth-callback/actions.ts
+++ src/app/auth-callback/actions.ts
@@ -7,7 +7,7 @@ export const getAuthStatus = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  if (!user?.id || !user?.email) {
+  if (!user?.id || !user.email) {
     throw new Error("Invalid user data");
   }
 
