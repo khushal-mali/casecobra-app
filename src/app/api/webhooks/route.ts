8ab6dcb206967236983b2170d7b986318d376f50
Khushal Mali <khushalmali8448@gmail.com>
2024-06-14 11:11:20 +0530

    added resend email in webhook

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -2,7 +2,11 @@ import { db } from "@/db";
 import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
-import type Stripe from "stripe";
+import Stripe from "stripe";
+import { Resend } from "resend";
+import OrderReceivedEmail from "@/components/emails/OrderReceivedEmail";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
@@ -10,7 +14,7 @@ export async function POST(req: Request) {
     const signature = headers().get("stripe-signature");
 
     if (!signature) {
-      return new Response("Invalid Signature", { status: 400 });
+      return new Response("Invalid signature", { status: 400 });
     }
 
     const event = stripe.webhooks.constructEvent(
@@ -32,13 +36,13 @@ export async function POST(req: Request) {
       };
 
       if (!userId || !orderId) {
-        throw new Error("Invalid request metadata.");
+        throw new Error("Invalid request metadata");
       }
 
       const billingAddress = session.customer_details!.address;
       const shippingAddress = session.shipping_details!.address;
 
-      await db.order.update({
+      const updatedOrder = await db.order.update({
         where: {
           id: orderId,
         },
@@ -51,7 +55,7 @@ export async function POST(req: Request) {
               country: shippingAddress!.country!,
               postalCode: shippingAddress!.postal_code!,
               street: shippingAddress!.line1!,
-              state: shippingAddress!.state!,
+              state: shippingAddress!.state,
             },
           },
           billingAddress: {
@@ -61,19 +65,38 @@ export async function POST(req: Request) {
               country: billingAddress!.country!,
               postalCode: billingAddress!.postal_code!,
               street: billingAddress!.line1!,
-              state: billingAddress!.state!,
+              state: billingAddress!.state,
             },
           },
         },
       });
+
+      await resend.emails.send({
+        from: "CaseCobra <khushalmali8448@gmail.com>",
+        to: [event.data.object.customer_details.email],
+        subject: "Thanks for your order!",
+        react: OrderReceivedEmail({
+          orderId,
+          orderDate: updatedOrder.createdAt.toLocaleDateString(),
+          // @ts-ignore
+          shippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAddress!.city!,
+            country: shippingAddress!.country!,
+            postalCode: shippingAddress!.postal_code!,
+            street: shippingAddress!.line1!,
+            state: shippingAddress!.state,
+          },
+        }),
+      });
     }
 
-    return NextResponse.json({ result: event, ok: true });
-  } catch (error) {
-    console.error(error);
+    return NextResponse.json({ ok: true, result: event });
+  } catch (err) {
+    console.error(err);
 
     return NextResponse.json(
-      { message: "Something went wrong!", ok: false },
+      { message: "Something went wrong", ok: false },
       { status: 500 }
     );
   }
